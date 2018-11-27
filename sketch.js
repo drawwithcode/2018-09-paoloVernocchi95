87210d3d1023f5f29a3f8f681f6d3794014f91e6
paoloVernocchi95 <43385409+paoloVernocchi95@users.noreply.github.com>
2018-11-27 23:45:09 +0100

    marted quasi

--- sketch.js
+++ sketch.js
@@ -3,57 +3,163 @@ var atalaricoLat;
 var atalaricoLong;
 var myMap;
 var canvas;
-var mappa=new Mappa("Leaflet");
-var atalaricoLat= 44.4303619;
-var atalaricoLong=12.1980816;
-var milanoLat=45.501902;
-var milanoLong= 9.165741;
-var boloLat=44.6360695;
-var boloLong= 10.4114535;
+var mappa=new Mappa("MapboxGL","pk.eyJ1IjoicGFvbG92cm4iLCJhIjoiY2pwMDVyMGhrMDIxaTNxcGpwaGRicTM2ZCJ9.o-_V3UFPnwIq0C4Q2IxHRg");
+var lombLat=45.501902;
+var lombLong= 9.165741;
+var emiliaLat=44.7017301;
+var emiliaLong= 10.5682279;
+var venetoLat=45.7322813;
+var venetoLong= 11.7407832;
+var lazioLat=41.8083258;
+var lazioLong= 12.617275;
+var campaniaLat=40.7456232;
+var campaniaLong= 13.6630268;
+var toscanaLat=43.351701;
+var toscanaLong= 10.90782;
+var siciliaLat=37.8350974;
+var siciliaLong= 13.9451173;
+var pugliaLat=41.0047401;
+var pugliaLong= 16.6059907;
+var abruzzoLat=42.2882456;
+var abruzzoLong= 14.3402867;
+var trentinoLat=46.3788459;
+var trentinoLong= 10.3085097;
+var romagnaLat=44.4123367;
+var roamgnaLong= 12.1654897;
+
+
+
 var options = {
-  lat:atalaricoLat,
-  lng:atalaricoLong,
-  zoom: 10,
-  style:"http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+  lat:lazioLat,
+  lng:lazioLong,
+  zoom: 5,
+  style:"mapbox://styles/paolovrn/cjp0bhzkz03ua2sp1f357jsdl"
 }
 
-var wallys;
 
 function preload(){
 myLoc= getCurrentPosition();
 }
-var fence;
-var polygon = [
-    {lat: 34.045303, lon: -118.334650},  // top left
-    {lat: 34.045252, lon: -118.334462},  // top right
-    {lat: 34.045131, lon: -118.334498},  // bottom right
-    {lat: 34.045185, lon: -118.334684},  // bottom left
-];
 function setup() {
   canvas=createCanvas(windowWidth,windowHeight);
   myMap = mappa.tileMap(options);
   myMap.overlay(canvas);
-  atalaricoLat= 44.4303619
-  atalaricoLong=12.1980816
+  textAlign(CENTER);
   //wallys = new Ball(44.4303619- 1*random(),12.1980816- 1*random(), 50);
-fence = new geoFenceCircle(44.979779, -93.325499, 0.05, insideTheFence, outsideTheFence, 'mi');
 
 }
-
+var hitlomb = false;
+var hitreggio = false;
+var hitveneto = false;
+var hitlazio = false;
+var hitcamp = false;
+var hitsic = false;
+var hitpuglia = false;
+var hittosc = false;
+var hittrent = false;
+var hitabruzz = false;
 function draw() {
 //  wallys.display();
   clear();
   fill(random(255),random(255),random(255));
 
-  var point = myMap.latLngToPixel(atalaricoLat,atalaricoLong);
-  var point2 = myMap.latLngToPixel(milanoLat,milanoLong);
-  var point3 = myMap.latLngToPixel(boloLat,boloLong);
-  fill(255,0,0,100);
-  beginShape();
-vertex(point.x,point.y);
-vertex(point2.x,point2.y);
-vertex(point3.x,point3.y);
-endShape(CLOSE);
+
+  var pointlomb = myMap.latLngToPixel(lombLat,lombLong);
+  var pointreggio = myMap.latLngToPixel(emiliaLat,emiliaLong);
+  var pointveneto = myMap.latLngToPixel(venetoLat,venetoLong);
+  var pointlazio = myMap.latLngToPixel(lazioLat,lazioLong);
+  var pointcamp = myMap.latLngToPixel(campaniaLat,campaniaLong);
+  var pointsic = myMap.latLngToPixel(siciliaLat,siciliaLong);
+  var pointpuglia = myMap.latLngToPixel(pugliaLat,pugliaLong);
+  var pointtoscana = myMap.latLngToPixel(toscanaLat,toscanaLong);
+  var pointtrento = myMap.latLngToPixel(trentinoLat,trentinoLong);
+  var pointabruzz = myMap.latLngToPixel(abruzzoLat,abruzzoLong);
+  var pointromagna = myMap.latLngToPixel(romagnaLat,roamgnaLong);
+  noStroke();
+  fill(255,0,0,0);
+    ellipse(pointlomb.x,pointlomb.y,90);
+    ellipse(pointreggio.x,pointreggio.y,90);
+      ellipse(pointveneto.x, pointveneto.y,90);
+      ellipse( pointlazio.x, pointlazio.y,90);
+        ellipse(pointcamp.x,pointcamp.y,90);
+        ellipse(pointsic.x,pointsic.y,90);
+          ellipse(pointpuglia.x,pointpuglia.y,90);
+          ellipse(pointtoscana.x,pointtoscana.y,90);
+            ellipse(pointtrento.x,pointtrento.y,90);
+            ellipse(pointabruzz.x,pointabruzz.y,90);
+            ellipse(pointromagna.x,pointromagna.y,90);
+            point(mouseX,mouseY);
+
+	hitlomb = collidePointCircle(mouseX,mouseY,pointlomb.x,pointlomb.y,90)
+  hitreggio = collidePointCircle(mouseX,mouseY,pointreggio.x,pointreggio.y,90)
+  hitveneto = collidePointCircle(mouseX,mouseY,pointveneto.x, pointveneto.y,90)
+  hitlazio = collidePointCircle(mouseX,mouseY,pointlazio.x, pointlazio.y,90)
+  hitcamp = collidePointCircle(mouseX,mouseY,pointcamp.x,pointcamp.y,90)
+  hitsic = collidePointCircle(mouseX,mouseY,pointsic.x,pointsic.y,90)
+  hitpuglia = collidePointCircle(mouseX,mouseY,pointpuglia.x,pointpuglia.y,90)
+  hittosc = collidePointCircle(mouseX,mouseY,pointtoscana.x,pointtoscana.y,90)
+  hittrent = collidePointCircle(mouseX,mouseY,pointtrento.x,pointtrento.y,90)
+  hitabruzz = collidePointCircle(mouseX,mouseY,pointabruzz.x,pointabruzz.y,90)
+  hitromagna = collidePointCircle(mouseX,mouseY,pointromagna.x,pointromagna.y,90)
+  if(hitlomb == true){
+    fill(255);
+    textSize(50);
+  text('ue pirla',mouseX,mouseY);
+  }
+  if(hitreggio == true){
+    fill(255);
+    textSize(50);
+  text('In dó vêt?',mouseX,mouseY);
+  }
+  if(hitveneto == true){
+    fill(255);
+    textSize(50);
+  text('ciao fioi come ea?',mouseX,mouseY);
+  }
+  if(hitlazio == true){
+    fill(255);
+    textSize(50);
+  text('SCIAO CÌ',mouseX,mouseY);
+  }
+  if(hitcamp == true){
+    fill(255);
+    textSize(50);
+  text('ué ué ',mouseX,mouseY);
+  }
+  if(hitsic == true){
+    fill(255);
+    textSize(50);
+  text('compà ',mouseX,mouseY);
+  }
+  if(hitpuglia == true){
+    fill(255);
+    textSize(50);
+  text('L muert e l stramuert d maam t! ',mouseX,mouseY);
+  }
+  if(hittosc == true){
+    fill(255);
+    textSize(50);
+  text('bona gigi , ci si vede ',mouseX,mouseY);
+  }
+  if(hittrent == true){
+    fill(255);
+    textSize(50);
+  text(' Tei hat finì de rozàr? ',mouseX,mouseY);
+  }
+  if(hitabruzz == true){
+    fill(255);
+    textSize(50);
+  text(' o buongiorn! ',mouseX,mouseY);
+  }
+  if(hitromagna == true){
+    fill(255);
+    textSize(50);
+  text(' ue tabac! ',mouseX,mouseY);
+  }
+fill(255);
+textSize(70)
+text("Scopri alcuni saluti italiani", windowWidth/2,windowHeight/8);
+
   //var point = myMap.latLngToPixel(atalaricoLat,atalaricoLong);
   //ellipse(point.x,point.y,30);
 }
@@ -77,17 +183,3 @@ endShape(CLOSE);
     ellipse(this.x, this.y, this.size);
   }
 }*/
-function insideTheFence(position){
-    print("INlat: " + position.latitude);
-    print("INlong: " + position.longitude);
-    print("user is inside of the fence")
-}
-
-function outsideTheFence(position){
-    print("OUTlat: " + position.latitude);
-    print("OUTlong: " + position.longitude);
-    print("user is outside of the fence")
-}
-function mousePressed(){
-	fence.clear();
-}
